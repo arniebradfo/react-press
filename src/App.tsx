1fc262925d4e42ca4efe640f1d05d89669e4696a
arniebradfo <arniebradfo@gmail.com>
2019-04-04 12:13:44 -0700

    typed scss modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.scss';
+import style from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={style.app}>
+        <header className={style["app-header"]}>
+          <img src={logo} className={style["app-logo"]} alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
           <a
-            className="App-link"
+            className={style["app-link"]}
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
