64946c5eadbf05daadc89a41cd55b2fd9adb922f
arniebradfo <arniebradfo@gmail.com>
2019-04-04 17:03:46 -0700

    revert most of the redux example changes

--- src/redux/actionTypes.ts
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = "ADD_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
-export const SET_FILTER = "SET_FILTER";
-
-export type IActionType = "ADD_TODO" | "TOGGLE_TODO" | "SET_FILTER";
