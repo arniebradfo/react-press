b67a6350deaaa4b9c18e444bff0430ecaf7ce30a
arniebradfo <arniebradfo@gmail.com>
2019-04-04 17:13:53 -0700

    adding redux

---
+++ src/redux/reducers/index.ts
@@ -0,0 +1,14 @@
+
+const initState = {
+	// global state includes:
+	// posts
+	// types
+	// pages
+	// ...everything available through the WP API
+}
+
+const rootReducer = (state=initState, action:any) => {
+	return state;
+} 
+
+export default rootReducer;
